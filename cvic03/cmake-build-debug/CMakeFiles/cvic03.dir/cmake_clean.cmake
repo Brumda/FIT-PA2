2c5374f1588baacf09a4cab39e79f5d691526f30
Martin Eliáš <74295617+Brumda@users.noreply.github.com>
2024-11-01 12:30:50 +0100

    Add files via upload

---
+++ cvic03/cmake-build-debug/CMakeFiles/cvic03.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cvic03.dir/test.cpp.o"
+  "CMakeFiles/cvic03.dir/test.cpp.o.d"
+  "cvic03"
+  "cvic03.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cvic03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cvic03/cmake-build-debug/CMakeFiles/cvic03.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cvic03.
+# This may be replaced when dependencies are built.
